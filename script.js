a5eee24618eeb9f8be782a7b6d4607dd0c1a6d96
Nick Stellato <nick@nickstellato.com>
2018-11-19 21:54:41 -0600

    added try catch blocks to script.js

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 (function() {
-  async function getJSON(url) {
-    const response = await fetch(url, {});
-    const json = await response.json();
-    const max = json.length;
-    return json[Math.floor(Math.random() * Math.floor(max))].word;
+  async function getRandomWord(url) {
+    try {
+      let response = await fetch(url, {});
+      let json = await response.json();
+      let max = json.length;
+      return json[Math.floor(Math.random() * Math.floor(max))].word;
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   async function generateProjectName() {
-    const word1 = await getJSON("https://api.datamuse.com/words?topics=color");
-    const word2 = await getJSON(
-      "https://api.datamuse.com/words?topics=topography"
-    );
-    const name = document.querySelector("#name");
-    name.innerHTML = `${word1} ${word2}`;
+    try {
+      const word1 = await getRandomWord(
+        "https://api.datamuse.com/words?topics=color"
+      );
+      const word2 = await getRandomWord(
+        "https://api.datamuse.com/words?topics=topography"
+      );
+      const name = document.querySelector("#name");
+      name.innerHTML = `${word1} ${word2}`;
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   const button = document.querySelector("#button");
